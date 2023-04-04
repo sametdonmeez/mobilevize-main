d16bcb45db8a09aa63385727f7ed79a0d152e282
sametdonmeez <sametdonmeez03@gmail.com>
2023-04-04 16:16:07 +0300

    step06

--- step_02/lib/app_state.dart
+++ step_02/lib/app_state.dart
@@ -3,12 +3,26 @@ import 'package:firebase_auth/firebase_auth.dart'
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart'; 
 import 'firebase_options.dart';
 
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
-    init();
+     Future<DocumentReference> addMessageToGuestBook(String message) {
+    if (!_loggedIn) {
+      throw Exception('Must be logged in');
+    }
+
+    return FirebaseFirestore.instance
+        .collection('guestbook')
+        .add(<String, dynamic>{
+      'text': message,
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+      'name': FirebaseAuth.instance.currentUser!.displayName,
+      'userId': FirebaseAuth.instance.currentUser!.uid,
+    });
+  }
+    
   }
 
   bool _loggedIn = false;
--- step_02/lib/home_page.dart
+++ step_02/lib/home_page.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart' // new
     hide EmailAuthProvider, PhoneAuthProvider;    // new
 import 'package:flutter/material.dart';           // new
-import 'package:provider/provider.dart';          // new
-
-import 'app_state.dart';                          // new
+import 'package:provider/provider.dart'; 
+import 'app_state.dart';           // new
+import 'guest_book.dart';
+                        // new
 import 'src/authentication.dart';                 // new
 import 'src/widgets.dart';
 
@@ -41,6 +42,20 @@ class HomePage extends StatelessWidget {
           const Header("What we'll be doing"),
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
+          ),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (appState.loggedIn) ...[
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                  ),
+                ],
+              ],
+            ),
           ),
         ],
       ),
